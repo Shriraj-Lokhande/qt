1b5e1216ae0c90177f68bca1bcb17a41b4483b74
Shriraj-Lokhande <shriraj446@gmail.com>
2024-07-10 08:38:40 +0530

    navbar qtify

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function App() {
+  return(
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
