088431ab81de73cdb5ff0d371467b83acd24d1de
Shriraj-Lokhande <154832281+Shriraj-Lokhande@users.noreply.github.com>
2025-01-25 15:05:44 +0530

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./Button.module.css"
 
-export const Button = ({children}) => {
+export const Button = () => {
   return (
     <button className={styles.btn} >Give Feedback</button>
   )
